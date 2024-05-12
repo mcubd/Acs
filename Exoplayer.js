dec6626a27cb994bf5499f00269f75c90f8efb0d
mcubd <106980362+mcubd@users.noreply.github.com>
2024-05-13 00:16:19 +0600

    V2  a5+// exoplayer landscapesensitive play,zoom,touch box,touch single skip

--- Exoplayer.js
+++ Exoplayer.js
@@ -1,5 +1,4 @@
-//PlayerControlView with SurfaceView
-
+V2  a5+// exoplayer landscapesensitive play,zoom,touch box,touch single skip
 <?xml version="1.0" encoding="utf-8"?>
 <RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
     xmlns:tools="http://schemas.android.com/tools"
@@ -8,11 +7,37 @@
     android:layout_height="match_parent"
     tools:context=".allow_access">
 
-    
-    <SurfaceView
+
+<!--    <SurfaceView-->
+<!--        android:id="@+id/surface_view"-->
+<!--        android:layout_centerCrop="true"-->
+<!--        android:layout_width="match_parent"-->
+<!--        android:layout_height="match_parent" />-->
+
+    <com.otaliastudios.zoom.ZoomSurfaceView
         android:id="@+id/surface_view"
         android:layout_width="match_parent"
-        android:layout_height="match_parent" />
+        android:layout_height="match_parent"
+        app:transformation="centerInside"
+        app:transformationGravity="auto"
+        app:alignment="center"
+        app:overScrollHorizontal="false"
+        app:overScrollVertical="false"
+        app:overPinchable="false"
+        app:horizontalPanEnabled="true"
+        app:verticalPanEnabled="true"
+        app:zoomEnabled="true"
+        app:flingEnabled="true"
+        app:scrollEnabled="true"
+        app:oneFingerScrollEnabled="true"
+        app:twoFingersScrollEnabled="true"
+        app:threeFingersScrollEnabled="true"
+        app:minZoom="1"
+        app:minZoomType="zoom"
+        app:maxZoom="8"
+        app:maxZoomType="zoom"
+        app:animationDuration="10"/>
+
 
 
     <androidx.media3.ui.PlayerControlView
@@ -23,47 +48,82 @@
 
         />
 
+    <View
+        android:id="@+id/touch_r"
+        android:layout_width="100dp"
+        android:layout_height="100dp"
+        android:background="@android:color/black"
+        android:layout_gravity="top|start"
+        android:layout_marginTop="10dp"
+        android:clickable="true"
+        android:focusable="true" />
+
+    <View
+        android:id="@+id/f"
+        android:layout_width="100dp"
+        android:layout_height="100dp"
+        android:layout_alignParentTop="true"
+        android:layout_alignParentRight="true"
+        android:background="@android:color/black"
+        android:layout_marginTop="10dp"
+        android:clickable="true"
+        android:focusable="true" />
+
 </RelativeLayout>
 
-//---------------------------------------------------------------------------------------
-
+//---------------------------------------------------------------------------------------------------
 package com.a.acs2;
 
 
 
 import static com.a.acs2.R.*;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.SurfaceView;
 import androidx.media3.common.Player;
+import androidx.media3.common.VideoSize;
 import androidx.media3.exoplayer.ExoPlayer;
 import androidx.media3.ui.PlayerControlView;
 import androidx.media3.ui.PlayerView;
 import androidx.media3.common.util.UnstableApi;
 import android.view.SurfaceHolder;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import androidx.media3.common.MediaItem;
+
+import com.otaliastudios.zoom.ZoomSurfaceView;
+
+import org.jetbrains.annotations.NotNull;
 
 @UnstableApi
 public class allow_access extends AppCompatActivity {
 
     private ExoPlayer mediaPlayer;
-    private SurfaceView surfaceView;
+    private ZoomSurfaceView surfaceView;
     private PlayerControlView playerControlView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
         setContentView(R.layout.activity_main);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        View touch_r = findViewById(R.id.touch_r);
+
 
         mediaPlayer = new ExoPlayer.Builder(this).build();
 
@@ -71,21 +131,29 @@ public class allow_access extends AppCompatActivity {
         playerControlView = findViewById(R.id.player_control_view);
 
         // Set up SurfaceView for video rendering
-        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+//        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+//            @Override
+//            public void surfaceCreated(SurfaceHolder holder) {
+//                mediaPlayer.setVideoSurface(holder.getSurface());
+//            }
+//            @Override
+//            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+//            @Override
+//            public void surfaceDestroyed(SurfaceHolder holder) {}
+//        });
+
+        surfaceView.addCallback(new ZoomSurfaceView.Callback() {
             @Override
-            public void surfaceCreated(SurfaceHolder holder) {
-                // Set surface for the media player
-                mediaPlayer.setVideoSurface(holder.getSurface());
+            public void onZoomSurfaceCreated(@NotNull ZoomSurfaceView view) {
+                Surface surface = view.getSurface();
+                mediaPlayer.setVideoSurface(surface);
+
+
+                // Use this surface for video players, camera preview, ...
             }
 
             @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-                // Not used
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-                // Not used
+            public void onZoomSurfaceDestroyed(@NotNull ZoomSurfaceView view) {
             }
         });
 
@@ -94,7 +162,17 @@ public class allow_access extends AppCompatActivity {
 
         // Prepare the media item
         MediaItem mediaItem = MediaItem.fromUri("https://flame.flameriser78.workers.dev/0:/mcubd/OTHERS/bng/5.mp4");
+//        MediaItem mediaItem = MediaItem.fromUri("https://flame.flameriser78.workers.dev/0:/mcubd/0331.mp4");
         mediaPlayer.setMediaItem(mediaItem);
+
+
+        mediaPlayer.addListener(new ExoPlayer.Listener() {
+            @Override
+            public void onVideoSizeChanged(@NonNull VideoSize videoSize) {
+                Toast.makeText(allow_access.this, "This is a toast message", Toast.LENGTH_SHORT).show();
+                surfaceView.setContentSize(videoSize.width, videoSize.height);
+            }
+        });
 
         // Start playback
         mediaPlayer.prepare();
@@ -113,6 +191,24 @@ public class allow_access extends AppCompatActivity {
         });
 
 
+        // Set onTouchListener for the view
+        touch_r.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Touching the view, show a toast
+                        showToast("Touched!");
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        // Released the touch, show another toast
+                        showToast("Touch Released!");
+                        return true;
+                }
+                return false;
+            }
+        });
+
 
     }
 
@@ -121,7 +217,11 @@ public class allow_access extends AppCompatActivity {
         super.onDestroy();
         mediaPlayer.release();
     }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() + 10000);
+
+
+    }
 }
-
-
-
